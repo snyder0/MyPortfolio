3d7c2e52feb434733139ae9412acc55daba66633
Snyder0 <luke.snyder@selu.edu>
2020-07-17 17:20:33 -0500

    Added AppBar | Added gretting text to homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "./components/AppBar";
 import Routes from "./router/Router";
 import Grid from "@material-ui/core/Grid";
 import "./App.css";
@@ -7,14 +8,19 @@ import "./App.css";
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    width: "100%"
   },
+  appBar: {
+    width: "100%"
+  }
 }));
 
 function App() {
   const classes = useStyles();
   return (
     <div className="App">
-      <Grid container className={classes.root} spacing={2}>
+      <Grid container className={classes.root}>
+        <AppBar className={classes.appBar} />
         <Routes />
       </Grid>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,43 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
-
+  root: {
+    minHeight: "90vh",
+    width: "100%",
+  },
+  skill: {
+    color: "#7FCD91",
+  },
 }));
 
 function Home() {
+  const classes = useStyles();
   return (
     <>
       <Grid item xs={12}>
-        <Grid container justify="center" spacing={1}>
-          <h1>Home</h1>
+        <Grid
+          container
+          className={classes.root}
+          justify="center"
+          alignItems="center"
+          spacing={1}
+        >
+          <Typography align="left" variant="h3">
+            <span className={classes.skill}>Hello,</span>
+            <br />
+            my name is
+            <br />
+            <Typography variant="h1">
+              Luke Snyder,
+            </Typography>
+            and I enjoy{" "}
+            <span className={classes.skill}>
+              web development.
+            </span>
+          </Typography>
         </Grid>
       </Grid>
     </>
